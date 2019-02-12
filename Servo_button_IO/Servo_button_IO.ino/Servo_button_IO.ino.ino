f2a6cbbb339c90d5f55c4a43834ca6b24393a00c
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-02-12 16:38:51 +0100

    Modificato codice Arduino

--- Servo_button_IO/Servo_button_IO.ino/Servo_button_IO.ino.ino
+++ Servo_button_IO/Servo_button_IO.ino/Servo_button_IO.ino.ino
@@ -9,12 +9,16 @@ int val;
 int buttonPin = 2;
 int lastState = 0;
 int buttonState = 0;
+int ledBlu = 12;
+int ledRed = 4;
 
 void setup() {
   Servo1.attach (8);
   Servo2.attach (7);
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(ledBlu, OUTPUT);
+  pinMode(ledRed, OUTPUT);
 
 }
 
@@ -32,10 +36,14 @@ void loop() {
 
   if (buttonState == 1) {
 
+digitalWrite(ledRed, HIGH);
+digitalWrite(ledBlu, LOW);
     Servo1.write(0);
     Servo2.write(0);
   }
   else {
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledBlu, HIGH);
     Servo1.write(val);
     Servo2.write(179 - val);
   }
